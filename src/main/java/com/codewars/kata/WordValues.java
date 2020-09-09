2191f28ecbcb2afdbdeb2e7a0d455c89fd0ea397
Ave <martinavelino91@gmail.com>
2020-09-09 18:54:33 +0200

    new kata: WordValues

---
+++ src/main/java/com/codewars/kata/WordValues.java
@@ -0,0 +1,30 @@
+package com.codewars.kata;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class WordValues {
+    public static int[] nameValue(String[] arr) {
+        List<Character> charArray = IntStream.rangeClosed('a', 'z')
+                .mapToObj(c -> (char) c).collect(Collectors.toList());
+        int[] resultValues = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            char[] chars = arr[i].toCharArray();
+            int value = 0;
+            for (char letter : chars) {
+                value = value + charArray.indexOf(letter) + 1;
+            }
+            resultValues[i] = value * (i + 1);
+        }
+        return resultValues;
+    }
+
+    public static int[] solution(String[] arr) {
+        int[] result = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            result[i] = arr[i].chars().filter(e -> e != ' ').map(e -> e - 96).sum() * (i + 1);
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/codewars/kata/WordValuesTest.java
@@ -0,0 +1,15 @@
+package com.codewars.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class WordValuesTest {
+
+    @Test
+    public void basicTests() {
+        assertArrayEquals(new int[]{6, 24}, WordValues.nameValue(new String[]{"abc", "abc abc"}));
+        assertArrayEquals(new int[]{88, 12, 225}, WordValues.nameValue(new String[]{"codewars", "abc", "xyz"}));
+        assertArrayEquals(new int[]{351, 282, 330}, WordValues.nameValue(new String[]{"abcdefghijklmnopqrstuvwxyz", "stamford bridge", "haskellers"}));
+    }
+}

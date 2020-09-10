f6c8386ed26d491120daf951d334748fbfc7c9e5
Ave <martinavelino91@gmail.com>
2020-09-10 12:34:41 +0200

    added new kata: MexicanWave

--- src/main/java/com/codewars/kata/WordValues.java
+++ src/main/java/com/codewars/kata/WordValues.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-class WordValues {
+class WordValues { //https://www.codewars.com/kata/598d91785d4ce3ec4f000018
     public static int[] nameValue(String[] arr) {
         List<Character> charArray = IntStream.rangeClosed('a', 'z')
                 .mapToObj(c -> (char) c).collect(Collectors.toList());
@@ -23,7 +23,7 @@ class WordValues {
     public static int[] solution(String[] arr) {
         int[] result = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
-            result[i] = arr[i].chars().filter(e -> e != ' ').map(e -> e - 96).sum() * (i + 1);
+            result[i] = arr[i].chars().filter(e -> e != ' ').map(e -> e - 96).sum() * (i + 1); // https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
         }
         return result;
     }
--- src/test/java/com/codewars/kata/WordValuesTest.java
+++ src/test/java/com/codewars/kata/WordValuesTest.java
@@ -8,7 +8,7 @@ class WordValuesTest {
 
     @Test
     public void basicTests() {
-        assertArrayEquals(new int[]{6, 24}, WordValues.nameValue(new String[]{"abc", "abc abc"}));
+        assertArrayEquals(new int[]{6, 24}, WordValues.solution(new String[]{"abc", "abc abc"}));
         assertArrayEquals(new int[]{88, 12, 225}, WordValues.nameValue(new String[]{"codewars", "abc", "xyz"}));
         assertArrayEquals(new int[]{351, 282, 330}, WordValues.nameValue(new String[]{"abcdefghijklmnopqrstuvwxyz", "stamford bridge", "haskellers"}));
     }
